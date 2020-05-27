ebe8d880788f324316000c1efbbe63319e89b413
Gina Fang <cat81050.au@gmail.com>
2020-05-28 00:46:06 +0800

    add head

--- products.py
+++ products.py
@@ -4,8 +4,14 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ') #如果使用者輸入q就不執行這行
+	price = int(price)
 	products.append([name, price])
 print(products)
 
 for product in products:
 	print(product[0], '的價格是', product[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f: #encoding 使用utf-8編碼
+	f.write('商品,價格\n')
+	for product in products:
+		f.write(product[0] + ',' + str(product[1]) + '\n') 
